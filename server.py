febfc8931d4c15e8bb95e1c1ecec0df6e3b1c4aa
SEUNGWOO JUNG <jsw9808@gmail.com>
2018-09-30 06:50:03 +0900

    route

--- server.py
+++ server.py
@@ -26,13 +26,23 @@ def login():
     else:
         return json.dumps({'error' : '아이디와 비밀번호를 확인하세요!'}, ensure_ascii = False)
 
+@app.route('/signin', methods = Consts.POST.value)
+def signIn():
+    try:
+        db.signIn(id = request.form[Consts.ID.value], password = request.form[Consts.PASSWORD.value], name = request.form[Consts.NAME.value],
+                  email = request.form[Consts.EMAIL.value], type = request.form[Consts.TYPE.value], birth = request.form[Consts.BIRTH.value],
+                  sex = request.form[Consts.SEX.value], access = request.form[Consts.ACCESS.value])
+        return json.dumps({Consts.ID.value : request.form[Consts.ID.value]})
+    except:
+        return json.dumps({'error' : '현제 데이터베이스 서버에 접속이 불안정합니다\n잠시후 다시 시도해 주세요!'})
+        
 def login_session(id):
     if sessiondb.isExist_session(id) == 1:
         sessiondb.drop_session(id)
     return sessiondb.create_session(id)
 
 def always_variable(info):
-    return info.form[Consts.ID.value], request.form[Consts.SESSION.value]
+    return info.form[Consts.ID.value], info.form[Consts.SESSION.value]
 
 def start_test_server():
     app.run(debug = True)
